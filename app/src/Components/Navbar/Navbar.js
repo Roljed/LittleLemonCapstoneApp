a9a66819d07b96d2a5ab18088d33f92ab88e1dce
Yaad Nahshon <ydnahshon@gmail.com>
2023-07-17 14:42:17 +0300

    fix: lemon logo position inline with the rest of the navigation links

--- app/src/Components/Navbar/Navbar.js
+++ app/src/Components/Navbar/Navbar.js
@@ -3,13 +3,13 @@ import { ReactComponent as Logo } from '../../assets/Logo.svg'
 
 import Home from '../../Pages/Home/Home';
 import About from '../../Pages/About/About';
-
+import './styles.css';
 
 const Navbar = () => {
     return (
         <>
-        <Logo/>
         <nav className="nav">
+            <Logo/>
             <Link to="/" className="nav-item">Home</Link>
             <Link to="/about" className="nav-item">About</Link>
             <Link to="/menu" className="nav-item">Menu</Link>
