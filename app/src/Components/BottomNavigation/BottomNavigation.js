20d121f131a61cfb00d80454541b60194f134913
Yaad Nahshon <ydnahshon@gmail.com>
2023-07-17 19:20:17 +0300

    feat: update footer

--- app/src/Components/BottomNavigation/BottomNavigation.js
+++ app/src/Components/BottomNavigation/BottomNavigation.js
@@ -1,12 +1,15 @@
-// import img from "../Assets/icons_assets/restauranfood.jpg"
+import './styles.css'
+import Restaurant from '../../assets/restaurant.jpg'
+
+
 const BottomNavigation = () => {
     return(
-        <section id="bottom-navigation" className="container-md">
+        <section className="bottom-navigation">
             <div className="row">
-                <div className="part img-part col-md-3">
-                    {/* <img src={img} alt="..."/> */}
+                <div className="part img-part">
+                    <img className='img-part' alt='Restaurant'src={Restaurant} />
                 </div>
-                <div className="part col-md-3">
+                <div className="part">
                     <h4>Doormat Navigation</h4>
                     <ul>
                         <li><a href="...">Home</a></li>
@@ -17,7 +20,7 @@ const BottomNavigation = () => {
                         <li><a href="...">Login</a></li>
                     </ul>
                 </div>
-                <div className="part col-md-3">
+                <div className="part">
                     <h4>Contact</h4>
                     <ul>
                         <li><a href="...">Adress</a></li>
@@ -25,7 +28,7 @@ const BottomNavigation = () => {
                         <li><a href="...">Email</a></li>
                     </ul>
                 </div>
-                <div className="part col-md-3">
+                <div className="part">
                     <h4>Social Media Links</h4>
                     <ul>
                         <li><a href="...">Adress</a></li>
