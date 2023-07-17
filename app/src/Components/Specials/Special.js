55051a686ec5b5196f9becb6d82a5850095e5706
Yaad Nahshon <ydnahshon@gmail.com>
2023-07-17 20:08:22 +0300

    feat: update special dish cards

--- app/src/Components/Specials/Special.js
+++ app/src/Components/Specials/Special.js
@@ -1,10 +1,11 @@
 import './styles.css';
+import DeliveryIcon from "../../assets/scooter.svg"
 
 const Special = (props) => {
     return (
         <div className="special-card">
             <heading>
-                <img className="special-dish" alt="special dish picture" src={props.imageSrc}/>
+                <img className="special-dish" alt="special dish" src={props.imageSrc}/>
                 <div className="dish-heading">
                     <h4 className="dish-name">{props.name}</h4>
                     <h4 className="dish-price">{props.price}</h4>
@@ -13,9 +14,9 @@ const Special = (props) => {
             <main className="dish-details">
                 <p>{props.description}</p>
             </main>
-            <footer>
+            <footer className="special-card-footer">
                 <p>Order a delivery</p>
-                <img className="delivery-icon" alt="delivery"/>
+                <img className="delivery-icon" alt="delivery" src={DeliveryIcon}/>
             </footer>
         </div>
     )
