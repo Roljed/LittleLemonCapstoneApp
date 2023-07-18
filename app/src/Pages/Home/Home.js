f5bdf3c0844597a3894dedd92e11b4f384a3981e
Yaad Nahshon <ydnahshon@gmail.com>
2023-07-18 11:48:47 +0300

    feat: update homepage with new components

--- app/src/Pages/Home/Home.js
+++ app/src/Pages/Home/Home.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { ReactComponent as MetaImage } from '../../assets/home icon.svg'
-import Hero from '../../Components/Hero/Hero'
+import CallToAction from '../../Components/CallToAction/CallToAction';
+import SpecialCards from '../../Components/Specials/SpecialCards';
+import CustomersSay from '../../Components/CustomersSay/CustomersSay';
+
+import './styles.css';
 
 function Home() {
     return (
         <>
-            <meta name="description" content="Little Lemon Restaurant home page"/>
-            <meta name="og:title" content="Little Lemon"/>
-            <meta name="og:description" content="some description"/>
-            <meta name="og:image" content={MetaImage}/>
-            <Hero />
+            <head>
+                <title>Little Lemon Website</title>
+                <meta name="description" content="Little Lemon Restaurant home page"/>
+                <meta name="og:title" content="Little Lemon"/>
+                <meta name="og:description" content="some description"/>
+                <meta name="og:image" content={MetaImage}/>
+            </head>
+            <body className='home'>
+                <CallToAction />
+                <SpecialCards />
+                <CustomersSay />
+            </body>
 
         </>
     )
