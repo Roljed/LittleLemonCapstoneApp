085ede5241149aa9ef7f736859f28c2b6289fa1f
Yaad Nahshon <ydnahshon@gmail.com>
2023-07-18 11:45:39 +0300

    feat: update hero section to call to action

--- app/src/Components/CallToAction/CallToAction.js
+++ app/src/Components/CallToAction/CallToAction.js
@@ -1,7 +1,7 @@
 import RestaurantFood from '../../assets/restauranfood.jpg'
 import './styles.css';
 
-const Hero = () => {
+const CallToAction = () => {
     return (
         <>
             <section className='hero'>
@@ -20,4 +20,4 @@ const Hero = () => {
     )
 }
 
-export default Hero
+export default CallToAction
